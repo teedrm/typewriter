b2e1c3619fa38dc3dbb5acde3f207c5ae3772ee2
Thy Nguyen <tee@Thys-iMac.local>
2022-11-22 19:42:44 -0700

    animated sentence

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,5 @@
 const sentence = "hello there from lighthouse labs";
 
-// for (const char of sentence) {
-
-//   setTimeout(() => {
-//     process.stdout.write(char);
-//   }, 1000); // <= 1s delay to make it noticeable. Can dial it down later.
-// }
-
 const typewriter = sentence => {
   for (let char = 0; char < sentence.length; char++) {
     
